069159afedd4ef3261a26dfaaac0a31851918b84
Adelçon Junior <adelconjr@outlook.com.br>
2022-06-25 20:03:51 -0300

    Criando a classe Conta e definindo atributos e métodos

---
+++ src/main/kotlin/dio/digitalinnovation/digiobank/Conta.kt
@@ -0,0 +1,17 @@
+package dio.digitalinnovation.digiobank
+
+import java.math.BigDecimal
+
+class Conta {
+    val agencia: String = ""
+    val numeroDaConta: String = ""
+    val saldo: BigDecimal = TODO()
+
+    fun deposito(valor: BigDecimal) {
+
+    }
+
+    fun saque(valor: BigDecimal) {
+
+    }
+}
